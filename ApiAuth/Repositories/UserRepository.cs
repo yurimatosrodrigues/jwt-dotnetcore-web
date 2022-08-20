5dcc52bdcd43a870e3ca1326489e607886d80afc
Yuri Matos Rodrigues <yuri.matos@phibra.com.br>
2022-08-19 23:59:28 -0300

    Criando model e repository

---
+++ ApiAuth/Repositories/UserRepository.cs
@@ -0,0 +1,14 @@
+﻿using ApiAuth.Models;
+
+namespace ApiAuth.Repositories
+{
+    public static class UserRepository
+    {
+        public static User Get(string username, string password)
+        {
+            var users = new List<User>();
+            users.Add(new User { Id = 1, Username = "Yuri", Password = "teste", Role = "" });
+            return null;
+        }
+    }
+}
