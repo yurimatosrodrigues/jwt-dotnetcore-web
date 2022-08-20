781ac951d032725834d801380ae7600de0bbaaa2
Yuri Matos Rodrigues <yuri.matos@phibra.com.br>
2022-08-20 00:16:31 -0300

    Validação do usuário (get)

--- ApiAuth/Repositories/UserRepository.cs
+++ ApiAuth/Repositories/UserRepository.cs
@@ -6,9 +6,16 @@ namespace ApiAuth.Repositories
     {
         public static User Get(string username, string password)
         {
-            var users = new List<User>();
-            users.Add(new User { Id = 1, Username = "Yuri", Password = "teste", Role = "" });
-            return null;
+            var users = new List<User>()
+            {
+                new User { Id = 1, Username = "yuri", Password = "teste", Role = "manager" },
+                new User { Id = 1, Username = "magna", Password = "magna", Role = "employee" }
+            };
+
+            return users
+                .FirstOrDefault(u => 
+                     u.Username.ToLower() == username.ToLower() && 
+                     u.Password == password)!;
         }
     }
 }
